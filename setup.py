8ddc9517dde8673c4b99c499c00e36902d638f1d
Chip Webber <chipwebber@gmail.com>
2012-03-16 14:04:12 -0500

    added new files

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='pssepath',
+        version='0.2',
+        description='A module to detect and setup paths required for Python for PSSE operation.',
+        author='Daniel Hillier',
+        packages=['pssepath']
+        )
