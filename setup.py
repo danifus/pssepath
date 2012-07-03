fc4cc37cc5313d3a3e7f1f9d7444f523113a2b37
Daniel Hillier <daniel@whit.com.au>
2012-07-03 17:32:54 +1000

    Update setup.py - Version 0.6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@
 from setuptools import setup
 
 setup(name='pssepath',
-        version='0.5',
+        version='0.6',
         description='A module to detect and setup paths required for Python for PSSE operation.',
         author='Daniel Hillier',
         py_modules=['pssepath']
